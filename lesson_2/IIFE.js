7b7e6e00b09ccfa473352c3c71aa8c3709de6e6e
Ian Lenny <ian.lenny@gmail.com>
2020-09-30 15:34:32 -0500

    updated exercises as of 29SEP20

--- lesson_2/IIFE.js
+++ lesson_2/IIFE.js
@@ -1,6 +1,12 @@
-(function count(number) {
-  console.log(number);
-  if (number !== 0) {
-        count(number - 1);
-  } 
-})(7);
+let bar = (function(start) {
+  let prod = start;
+  return function (factor) {
+    prod *= factor;
+    return prod;
+  };
+})(2);
+
+let result = bar(3);
+result += bar(4);
+result += bar(5);
+console.log(result);
--- lesson_2/practice.js
+++ lesson_2/practice.js
@@ -1,77 +1,13 @@
-cont professionalMixin = {
-  invoice() {
-
-  },
-
-  payTaxes() {
-
-  },
+function foo(one, two, three) {
+  return {
+    bar: one,
+    baz: two,
+    qux: three,
+  };
 }
 
+let { baz, qux, bar } = foo(1, 2, 3);
 
-function Person(firstName, lastName, age, gender) {
-  this.firstName = firstName;
-  this.lastName = lastName;
-  this.age = age;
-  this.gender = gender;
-}
-
-Person.prototype.fullName = function() {
-  return this.firstName + ' ' + this.lastName;
-};
-
-Person.prototype.communicate = function() {
-  console.log('Communicating');
-};
-
-Person.prototype.eat = function() {
-  console.log('Eating');
-};
-
-Person.prototype.sleep = function() {
-  console.log('Sleeping');
-};
-
-function Doctor(firstName, lastName, age, gender, specialization) {
-  Person.call(this, firstName, lastName, age, gender);
-  this.specialization = specialization;
-}
-
-Doctor.prototype = Object.create(Person.prototype);
-Doctor.prototype.diagnose = function() {
-  console.log('Diagnosing');
-};
-Doctor.prototype.constructor = Doctor;
-
-function Professor(firstName, lastName, age, gender, subject) {
-  Person.call(this, firstName, lastName, age, gender);
-  this.subject = subject;
-}
-
-Professor.prototype = Object.create(Person.prototype);
-Professor.prototype.teach = function() {
-  console.log('Teaching');
-};
-Professor.prototype.constructor = Professor;
-
-function Student(firstName, lastName, age, gender, degree) {
-  Person.call(this, firstName, lastName, age, gender);
-  this.degree = degree;
-}
-
-Student.prototype = Object.create(Person.prototype);
-Student.prototype.study = function() {
-  console.log('Studying');
-};
-Student.prototype.constructor = Student;
-
-function GraduateStudent(firstName, lastName, age, gender, degree, graduateDegree) {
-  Student.call(this, firstName, lastName, age, gender, degree);
-  this.graduateDegree = graduateDegree;
-}
-
-GraduateStudent.prototype = Object.create(Student.prototype);
-GraduateStudent.prototype.research = function() {
-  console.log('Researching');
-};
-GraduateStudent.prototype.constructor = GraduateStudent;
+let baz = foo.bar;
+let qux = foo.qux;
+let bar = foo.bar;
